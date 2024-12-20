e28e5d97bda6883290b8871b52396804c949bcbe
Surya K <kumarkugan26@gmail.com>
2024-12-20 10:36:13 +0530

    Added new methods

--- Test.java
+++ Test.java
@@ -1,5 +1,18 @@
 public class Test {
     public static void main(String[] args) {
-     System.out.println("HEllo");
+        System.out.println("Hello, World!");
+
+        Test test = new Test();
+
+        test.sayHello();
     }
+
+
+
+    public void sayHello() {
+        System.out.println("Hello from the new method!");
+    }
+
+    
+    
 }
